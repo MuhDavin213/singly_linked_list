b3a1f77bbaecfc03e3c8447778c9ac81e3a27dcd
MuhDavin213 <davin.new58@gmail.com>
2022-11-07 16:57:42 +0700

    add class addnote

--- Program.cs
+++ Program.cs
@@ -21,9 +21,17 @@ namespace singly_linked_list
         {
             START = null;
         }
-        public void addNote()
+        public void addNote() // add a node in the list
         {
-
+            int rollNo;
+            string nm;
+            Console.Write();
+            rollNo = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\nEnter the roll name of the student: ");
+            nm = Console.ReadLine();
+            node newnode = new node();
+            newnode.rollnumber = rollNo;
+            newnode.name = nm;
         }
     }
     class Program
